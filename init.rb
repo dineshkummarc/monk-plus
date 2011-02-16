337635ace9bd40cbb06426204ef0c19b4f408a94
Rico Sta. Cruz <rico@ambiescent.com>
2011-02-16 11:16:03 +0800

    Add gem version requirements in init.rb.

--- init.rb
+++ init.rb
@@ -2,13 +2,15 @@ $:.unshift(*Dir["./vendor/*/lib"])
 
 begin
   require "rubygems"  if RUBY_VERSION < "1.9"
+  gem 'sinatra', '~> 1.1'
+  gem 'haml', '~> 3.0'
   require "sinatra/base"
   require "rtopia"
   require "sinatra/content_for"
   require "jsfiles"
   require "user_agent"
 rescue LoadError => e
-  $stderr.write "Not all gems were able to load. (#{e.message})\n"
+  $stderr.write "Not all gems were able to load. (#{e.message.strip})\n"
   $stderr.write "Do `monk install` first, or install the gems in .gems yourself.\n"
   exit
 end
